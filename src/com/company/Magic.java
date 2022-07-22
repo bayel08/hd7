9f12852d5044b0b182af17ef32cb83f99181f191
unknown <egemberdievbayel199@gmail.com>
2022-07-22 17:20:58 +0600

    Initial commit

---
+++ src/com/company/Magic.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Magic extends Hero{
+    public Magic(int health, int damage, String superPowersType) {
+        super(health, damage, superPowersType);
+    }
+
+    @Override
+    public void applySuperAbility(String superAbilityType) {
+        System.out.println("Magic применил суперспособность: " + getSuperPowersType());
+    }
+    public String info (){
+        return super.info();
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Magic magic = new Magic(240, 15, "BOOST");
+        Medic medic = new Medic(220, 10, "Heal Points", 5);
+        Warrior warrior = new Warrior(250, 20, "CRITICAL DAMAGE");
+	    Hero[] havingSuperAbility = {magic, medic, warrior};
+            for (int i = 0; i < havingSuperAbility.length; i++) {
+                havingSuperAbility[i].applySuperAbility("s");
+                System.out.println(havingSuperAbility[i].info());
+                if(havingSuperAbility[i] == medic){
+                    System.out.println(medic.increaseExperience());
+                }
+        }
+    }
+}
